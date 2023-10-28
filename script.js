caf5a74ac726a4a8ec2c1772f6b345b40c60cb7d
Anush-Nair <144286708+Anush-Nair@users.noreply.github.com>
2023-10-28 17:05:00 -0500

    Basic layout and script done

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('tipCalculatorForm');
+const billTotalInput = document.getElementById('billTotal');
+const tipPercentageInput = document.getElementById('tipPercentage');
+const tipSliderInput = document.getElementById('tipSlider');
+const tipAmountInput = document.getElementById('tipAmount');
+const totalWithTipInput = document.getElementById('totalWithTip');
+
+form.addEventListener('input', updateTipCalculator);
+
+function updateTipCalculator() {
+    const billTotal = parseFloat(billTotalInput.value);
+    const tipPercentage = parseInt(tipSliderInput.value);
+                
+    if (isNaN(billTotal)) {
+        alert('Please enter a valid number for Bill Total.');
+        return;
+    }
+
+    const tipAmount = (billTotal * tipPercentage) / 100;
+    const totalWithTip = billTotal + tipAmount;
+    tipPercentageInput.value = tipPercentage + "%";
+    tipAmountInput.value = tipAmount.toFixed(2);
+    totalWithTipInput.value = totalWithTip.toFixed(2);
+}
